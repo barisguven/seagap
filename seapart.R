dda4416e8a9051028988532b645602cc9b141db9
barisguven <barisguven1@gmail.com>
2025-01-28 19:23:15 +0300

    pick first seat number  mod 5 is 0, 1, or 3

--- seapart.R
+++ seapart.R
@@ -1,41 +1,32 @@
 set.seed(345)
 
-#num_of_line_five = 1
-#num_of_line_ten = 0
-seat_cap = 10
+seat_cap = 15
 
-# arada bir boşluk olan maximum boş koltuk sayısı 
-# max_empty = if (seat_cap %% 2 == 1) {
-#   seat_cap * 0.6
-# } else {
-#   seat_cap * 0.5
-# }
-
-# tümüyle boş salon
+# başlamak için tümüyle boş salon
 seat_status = rep(TRUE, times = seat_cap)
-seat_numbers = c(1:seat_cap)
+seats = c(1:seat_cap)
 
-# Mod 5'e göre 1, 3, ve 0 olan koltuk numaraları; daha sonra!!!
-odd_seats = seq(1, seat_cap, 2)
-max_empty = length(odd_seats)
+# Mod 5'e göre 1, 3, ve 0 olan koltuk numaraları
+seats_mod5 = seats[(seats %% 5) %in% c(0, 1, 3)]
+max_empty = length(seats_mod5)
 
 # Koltuk seçme fonksiyonu
-pick_seat = function(type = "odd", odd_seat_status) {
+pick_seat = function(type = "odd", mod5_seat_status) {
 
   if (type == "odd") {
-    #odd_seat_status = seat_status[odd_seats]
-    empty_odd_seats = odd_seats[odd_seat_status]
+    
+    empty_seats_mod5 = seats_mod5[mod5_seat_status]
 
-    if (length(empty_odd_seats) > 1) {
-      seat_pick = sample(empty_odd_seats, 1)
+    if (length(empty_seats_mod5) > 1) {
+      seat_pick = sample(empty_seats_mod5, 1)
     } else {
-      seat_pick = empty_odd_seats[1]
+      seat_pick = empty_seats_mod5[1]
     }
 
     return(seat_pick)
   } else {
 
-    empty_seats = seat_numbers[seat_status]
+    empty_seats = seats[seat_status]
 
     if (length(empty_seats) > 1) {
       return(sample(empty_seats, 1))
@@ -48,14 +39,16 @@ pick_seat = function(type = "odd", odd_seat_status) {
 seat_picker = function() {
   if (sum(seat_status) > 0) {
 
-    odd_seat_status = seat_status[odd_seats]
+    mod5_seat_status = seat_status[seats_mod5]
 
-    if (sum(odd_seat_status) > 0) {
-      seat_pick = pick_seat("odd", odd_seat_status)
+    if (sum(mod5_seat_status) > 0) {
+      seat_pick = pick_seat("odd", mod5_seat_status)
     } else {
       seat_pick = pick_seat("any")
     }
+
     return(seat_pick)
+
   } else {
     return(-1)
   }
