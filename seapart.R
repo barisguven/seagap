499674565ca8061148664289a6c18b6bb81cad7c
barisguven <barisguven1@gmail.com>
2025-01-29 15:53:50 +0300

    make seat_picker work with blocks and create situate function

--- seapart.R
+++ seapart.R
@@ -3,30 +3,40 @@ library(dplyr) # tibble oluşturmak için
 
 # Bu fonksiyon ilk önce iki kullanıcı arasında bir boşluk kalacak şekilde tesadüfi olarak koltuk numarası belirler. Bu tip koltukların tamamı dolduktan sonra kalan koltuklar arasından yine tesadüfi olarak koltuk numarası belirler.
 
-seapart = function(seat_cap = 50, users = seat_cap * 0.5, seed = 321) {
+seapart = function(n = 10, users = 20 * n * 0.5, seed = 321) {
   set.seed(seed)
-  seat_cap = seat_cap
+
+  block1 = c(1:(n * 5)) # ilk beşlik blok
+  block2 = c((5 * n + 1):(15 * n)) # onluk blok
+  block3 = c((15 * n + 1):(20 * n)) # ikinci beşlik blok
+  block1_3 = c(block1, block3)
+
+  seat_cap = 20 * n
   users = users
 
   # başlamak için tümüyle boş salon
+  seats = c(block1, block2, block3)
   seat_status = rep(TRUE, times = seat_cap)
-  seats = c(1:seat_cap)
 
-  # Mod 5'e göre 1, 3, ve 0 olan koltuk numaraları
-  seats_mod5 = seats[(seats %% 5) %in% c(0, 1, 3)]
-  max_empty = length(seats_mod5)
+  # Mod 5'e göre 1, 3 ve 0 olan beşli blok koltuk numaraları
+  seats_mod5 = block1_3[(block1_3 %% 5) %in% c(0, 1, 3)]
+  # Mod 5'e göre 2, 4, 6, 8 ve 0 olan onlu blok koltuk numaraları
+  seats_mod10 = block2[(block2 %% 10) %in% c(0, 2, 4, 6, 8)]
+
+  seats_mod5_10 = c(seats_mod5, seats_mod10)
+  max_empty = length(seats_mod5_10)
 
   # Koltuk seçme fonksiyonları
-  pick_seat = function(type = "mod5", mod5_seat_status) {
+  pick_seat = function(type = "mod5_10", mod5_10_seat_status) {
 
-    if (type == "mod5") {
+    if (type == "mod5_10") {
       
-      empty_seats_mod5 = seats_mod5[mod5_seat_status]
+      empty_seats_mod5_10 = seats_mod5_10[mod5_10_seat_status]
 
-      if (length(empty_seats_mod5) > 1) {
-        seat_pick = sample(empty_seats_mod5, 1)
+      if (length(empty_seats_mod5_10) > 1) {
+        seat_pick = sample(empty_seats_mod5_10, 1)
       } else {
-        seat_pick = empty_seats_mod5[1]
+        seat_pick = empty_seats_mod5_10[1]
       }
 
       return(seat_pick)
@@ -45,10 +55,10 @@ seapart = function(seat_cap = 50, users = seat_cap * 0.5, seed = 321) {
   seat_picker = function() {
     if (sum(seat_status) > 0) {
 
-      mod5_seat_status = seat_status[seats_mod5]
+      mod5_10_seat_status = seat_status[seats_mod5_10]
 
-      if (sum(mod5_seat_status) > 0) {
-        seat_pick = pick_seat("mod5", mod5_seat_status)
+      if (sum(mod5_10_seat_status) > 0) {
+        seat_pick = pick_seat("mod5", mod5_10_seat_status)
       } else {
         seat_pick = pick_seat("any")
       }
@@ -83,8 +93,10 @@ seapart = function(seat_cap = 50, users = seat_cap * 0.5, seed = 321) {
 
       if (i == max_empty) {
         cat_stars()
-        cat("Mod 5'e göre 0, 1, ve 3 olan koltukların tamamı dolu!", "\n")
-        cat("Diğer numaralara geçiliyor...", "\n")
+        cat("1. ve 3. bloklarda mod 5'e göre 0, 1 ve 3 olan ve", 
+            "2. blokta mod 10'a göre 0, 2, 4, 6 ve 8 olan",
+            "koltukların tamamı dolu!", 
+            "Diğer numaralara geçiliyor...", sep = "\n")
         cat_stars()
       }
     } else {
@@ -93,52 +105,65 @@ seapart = function(seat_cap = 50, users = seat_cap * 0.5, seed = 321) {
     }
   }
 
-  cat(paste("Kalan boş koltuk sayısı: ", sum(seat_status)))
+  cat("Kalan boş koltuk sayısı:", sum(seat_status))
 
-  # Oturma verisi oluşturma
-  seat_y_grid = rep(0, seat_cap)
-  seat_y = rep(NA, length(seat_order))
+  # Grid ve oturma verisi oluşturma fonksiyonu
+  situate = function(seats){
 
-  seat_x_grid = c(1:seat_cap) %% 5
-  seat_x_grid[which(seat_x_grid == 0)] = 5
+    seat_x = rep(0, length(seats))
 
-  seat_x = seat_order %% 5
-  seat_x[which(seat_x == 0)] = 5
+    for (i in 1:length(seats)) {
+      if (seats[i] %in% block1){
+        seat_x[i] = seats[i] %% 5
+        if (seat_x[i] == 0) {seat_x[i] = 5}
+      } else if (seats[i] %in% block2) {
+        seat_x[i] = seats[i] %% 10
+        if (seat_x[i] == 0) {seat_x[i] = 10}
+        seat_x[i] = seat_x[i] + 6
+      } else {
+        seat_x[i] = seats[i] %% 5
+        if (seat_x[i] == 0) {seat_x[i] = 5}
+        seat_x[i]  = seat_x[i] + 17
+      }
+    }
 
-  for (i in 1:length(seat_order)) {
-    if (!is.na(seat_order[i])) {
-      for (j in 1:(seat_cap/5)) {
-        in_interval = seat_order[i] > (j - 1) * 5 & seat_order[i] <= j * 5
-        if (in_interval) {
-          seat_y[i] = j
+    seat_y = rep(0, length(seats))
+  
+    for (i in 1:length(seats)) {
+      if (seats[i] %in% block1) {
+        for (j in 1:n) {
+          in_interval = (seats[i] > (j - 1) * 5) & (seats[i] <= j * 5)
+          if (in_interval) {
+            seat_y[i] = j
+          }
+        }
+      } else if (seats[i] %in% block2) {
+        for (j in 1:n) {
+          in_interval = (seats[i] > 5 * n + (j - 1) * 10) & (seats[i] <= 5 * n + j * 10)
+          if (in_interval) {
+            seat_y[i] = j
+          }
+        }
+      } else {
+        for (j in 1:n) {
+          in_interval = (seats[i] > 15 * n + (j - 1) * 5) & (seats[i] <= 15 * n + j * 5)
+          if (in_interval) {
+            seat_y[i] = j
+          }
         }
       }
     }
+    return(tibble(seat_x, seat_y))
   }
 
-  # grid
-  for (i in 1:length(seats)) {
-    for (j in 1:(seat_cap/5)) {
-      in_interval = seats[i] > (j - 1) * 5 & seats[i] <= j * 5
-      if (in_interval) {
-        seat_y_grid[i] = j
-      }
-    }
-  }
-
-  seating_data = tibble(
-    seats,
-    seat_x_grid,
-    seat_y_grid,
-    seat_order,
-    seat_x,
-    seat_y
-  )
+  grid = situate(seats)
+  seating_data = situate(seat_order)
+  seating_data = bind_cols(grid, seating_data)
 
   return(seating_data)
 }
 
-seating_data = seapart(seat_cap = 50, users = 15)
+seating_data = seapart(n = 10, users = 130)
 
 seating_data |>
   ggplot(aes(seat_x_grid, seat_y_grid, label = seats)) +
