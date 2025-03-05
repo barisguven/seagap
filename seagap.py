e0754341bb72a1558b4fd909f6e0314969ad90e1
barisguven <barisguven1@gmail.com>
2025-03-05 16:06:05 +0300

    finish up the main part of the function

--- seagap.py
+++ seagap.py
@@ -1,21 +1,20 @@
-# Bu fonksiyon ilk önce iki kullanıcı arasında bir boşluk kalacak şekilde, bu tip koltukların tamamı dolduktan sonra da kalan koltuklar arasından rastlantısal olarak koltuk numarası belirler.
-
 import numpy as np
 
-def seagap(sira_sayisi=10, kullanici_sayisi=10*sira_sayisi, seed=321, verbose=True):
+def seagap(sira_sayisi=10, kullanici_sayisi=10*10, seed=321, verbose=True):
+  """
+  Bu fonksiyon ilk önce iki kullanıcı arasında bir boşluk kalacak şekilde, bu tip koltukların tamamı dolduktan sonra da kalan koltuklar arasından rastlantısal olarak koltuk numarası belirler.
+  """
 
   # Add controls later
 
-  #seed=321
-  #sira_sayisi = 10
+
   rg = np.random.default_rng(seed)
-  #kullanici_sayisi = 100
   n_row = sira_sayisi
   n_users = kullanici_sayisi
 
-  block1 = np.arange(1, n*5 + 1, 1)
-  block2 = np.arange(n*5 + 1, 15*n + 1, 1)
-  block3 = np.arange(15*n + 1, 20*n + 1, 1)
+  block1 = np.arange(1, n_row*5 + 1, 1)
+  block2 = np.arange(n_row*5 + 1, 15*n_row + 1, 1)
+  block3 = np.arange(15*n_row + 1, 20*n_row + 1, 1)
   block1_3 = np.hstack((block1, block3))
 
   seat_cap = 20 * n_row
@@ -26,24 +25,27 @@ def seagap(sira_sayisi=10, kullanici_sayisi=10*sira_sayisi, seed=321, verbose=Tr
 
   # Mod 5'e göre 1, 3 ve 0 olan beşli blok koltuk numaraları
   block1_3_mod5 = block1_3[np.isin(block1_3 % 5, [0, 1, 3])]
+
   # Mod 5'e göre 2, 4, 6, 8 ve 0 olan onlu blok koltuk numaraları
   # Ya da çift sayılar
   block2_mod10 = block2[block2 % 2 == 0]
 
-  seats_mod5_10 = np.hstack((block1_3_mod5, block2_mod10))
-  max_empty = len(seats_mod5_10)
+  seats_w_gap = np.hstack((block1_3_mod5, block2_mod10))
+  max_empty = len(seats_w_gap)
 
   # Koltuk seçme fonksiyonları
-  def pick_seat(mod5_10_seat_status, type='mod5_10'):
+  def pick_seat(gap_status, gap):
     """
-    Koltuk seçen fonksiyon.
+    Raslantısal koltuk seçen fonksiyon.
+
+    Eğer gap=True ise, bir boşluk bırakacak şekilde, değilse herhangi bir koltuk numarası seçer. 
     """
-    if type == "mod5_10":
-      empty_seats_mod5_10 = seats_mod5_10[mod5_10_seat_status]
-      if (len(empty_seats_mod5_10) > 1):
-        seat_pick = empty_seats_mod5_10[rg.integers(0, len(empty_seats_mod5_10), 1)][0]
+    if gap:
+      empty_seats_w_gap = seats_w_gap[gap_status]
+      if (len(empty_seats_w_gap) > 1):
+        seat_pick = empty_seats_w_gap[rg.integers(0, len(empty_seats_w_gap), 1)][0]
       else:
-        seat_pick = empty_seats_mod5_10[0]
+        seat_pick = empty_seats_w_gap[0]
       return seat_pick
     else:
       empty_seats = seats[seat_status]
@@ -55,13 +57,15 @@ def seagap(sira_sayisi=10, kullanici_sayisi=10*sira_sayisi, seed=321, verbose=Tr
   def seat_picker():
     """
     Koltuk seçme talimatı veren fonksiyon.
+
+    Boş koltuk varsa koltuk seçmek için pick_seat() fonksiyonunu çağırır ve seçilen koltuk numarasını geri gönderir. Yoksa -1 geri gönderir.
     """
     if sum(seat_status) > 0:
-      mod5_10_seat_status = seat_status[seats_mod5_10 - 1]
-      if sum(mod5_10_seat_status) > 0:
-        seat_pick = pick_seat(mod5_10_seat_status)
+      gap_status = seat_status[seats_w_gap - 1]
+      if sum(gap_status) > 0:
+        seat_pick = pick_seat(gap_status, gap=True)
       else:
-        seat_pick = pick_seat(mod5_10_seat_status, type='any')
+        seat_pick = pick_seat(gap_status, gap=False)
       return seat_pick
     else:
       return -1
@@ -70,19 +74,24 @@ def seagap(sira_sayisi=10, kullanici_sayisi=10*sira_sayisi, seed=321, verbose=Tr
     print(50 * "*")
     print("Toplam başlangıç boş koltuk sayısı: ", seat_cap)
     print("Bir boşluklu maximum koltuk sayısı: ", max_empty)
+    print("Kullanıcı sayısı: ", n_users)
+    print(50 * "*")
 
-  # Koltuk seçme
+  # Seçilen koltukları tutacak liste
   seat_order = [np.nan] * seat_cap
 
+  # Sıfırdan büyük kullanıcı sayıları içim koltuk seçme
   if n_users != 0:
     for user in range(1, n_users + 1, 1):
       seat_pick = seat_picker()
       
-      if (seat_pick > 0):
+      if seat_pick > 0:
         seat_status[seat_pick - 1] = False
         
         if verbose:
-          print(user, ":", "Koltuk numaranız: ", seat_pick, sep="")
+          print(user, ". kullanıcı", " --> ", "Koltuk numaranız: ", seat_pick, sep="")
+          if user == seat_cap:
+            print("Kalan boş koltuk sayısı:", sum(seat_status))
 
         if user <= seat_cap:
           seat_order[user - 1] = seat_pick
@@ -91,11 +100,8 @@ def seagap(sira_sayisi=10, kullanici_sayisi=10*sira_sayisi, seed=321, verbose=Tr
               print(50 * "*")
               print("1. ve 3. bloklarda mod 5'e göre 0, 1 ve 3 olan ve", "2. blokta mod 10'a göre 0, 2, 4, 6 ve 8 olan", "koltukların tamamı dolu!", "Diğer numaralara geçiliyor...", sep = "\n")
               print(50 * "*") 
-        else:
-          print("200'den fazla kullanici var!!!!")
-          if verbose:
+      elif seat_pick == -1:
+        if verbose:
+          if user == seat_cap + 1:
             print(50 * "*")
-            print(user, ": ", "Üzgünüz, şu anda hiç boş koltuğumuz yok.", sep = "")
-
-  if verbose:
-    print("Kalan boş koltuk sayısı:", sum(seat_status))
+          print(user, ": ", "Üzgünüz, şu anda hiç boş koltuğumuz yok.", sep = "")
