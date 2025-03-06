7f9ad5afef97698bf356ebc4529c058002a4871d
barisguven <barisguven1@gmail.com>
2025-03-06 11:12:32 +0300

    finalize the function

--- seaplot.py
+++ seaplot.py
@@ -1,18 +1,28 @@
 import matplotlib.pyplot as plt
 
-# def seaplot(seating_data):
+def seaplot(seating_data):
+  """Koltuk numaralarını görselleştiren fonksiyon"""
+  
+  fig, ax = plt.subplots()
 
-fig, ax = plt.subplots()
+  ax.set_facecolor("lightgrey")
+  ax.tick_params(
+    axis='both',
+    which='both',
+    bottom=False,
+    left=False,
+    labelbottom=False,
+    labelleft=False
+  )
+  ax.set_title("Raslantısal oturma deseni - " + 
+               str(seating_data.   seat_order.count()) + 
+               " kullanıcı")
 
-ax.scatter(
-  "seat_x_grid", "seat_y_grid", data=grid, marker="s",
-  c="gray")
+  ax.scatter("seat_x_grid", "seat_y_grid", data=seating_data, marker="s", 
+             s=400, facecolor="lightgrey", edgecolors="black")
 
-for x, y, z in zip(grid.seat_x_grid, grid.seat_y_grid, grid.seats):
-  ax.annotate("%s" %z, xy=(x, y), xytext=(x, y), textcoords='data')
+  ax.scatter("seat_x", "seat_y", data=seating_data, marker="s", 
+             s=400, facecolor="rosybrown", edgecolors="black")
 
-
-ax.scatter("seat_x", "seat_y", data=grid, marker="s",
-c="rosybrown")
-
-ax.set_title("Raslantısal oturma deseni")
+  for x, y, s in zip(seating_data.seat_x_grid, seating_data.seat_y_grid, seating_data.seats):
+    ax.text(x, y, s, ha="center", va="center")
