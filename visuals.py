9b12bfb7aae4f2dcccc878fc185bc6caa278f606
barisguven <barisguven1@gmail.com>
2025-03-06 11:13:05 +0300

    add lines for different user number cases

---
+++ visuals.py
@@ -0,0 +1,7 @@
+# Programın görsel çıktıları
+seaplot(seagap(sira_sayisi=10, kullanici_sayisi=0))
+seaplot(seagap(sira_sayisi=10, kullanici_sayisi=70))
+seaplot(seagap(sira_sayisi=10, kullanici_sayisi=90))
+seaplot(seagap(sira_sayisi=10, kullanici_sayisi=110))
+seaplot(seagap(sira_sayisi=10, kullanici_sayisi=200))
+seaplot(seagap(sira_sayisi=2, kullanici_sayisi=0))
